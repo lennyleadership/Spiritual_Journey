d2a27e63f2e8fb3a0d2e86050a61b2b587e27a3d
lennyzhlin <zh.lenny.lin@gmail.com>
2022-07-30 21:10:33 -0700

    2022.07.30 9:10pm

--- Execution codes.R
+++ Execution codes.R
@@ -42,13 +42,14 @@ blogdown::find_hugo()
 
 # Step #1: Back up the folder 'content'.
 # Step #2: Delete the damaged repository at Github
-# Settings -> Danger Zone -> Delete this repository
+#          Settings -> Danger Zone -> Delete this repository
 # Step #3: Recreate the repository with exactly identical name.
 
 # Step #4: Create a new project at RStudio.
 # new project -> Version control -> Git -> Repository URL -> paste the URL, create project as subdirectory of ...
 # in Console, type:
 #  blogdown::new_site(theme = "wangchucheng/hugo-eureka")
+# Note: Donot copy and paste Execution codes.R yet.
 
 # Remove:
 # any pre-installed info in directory 'content', 'public', 
@@ -62,7 +63,7 @@ blogdown::find_hugo()
 
 #----
 # the image of the website is stored in D:\A_Blog\spiritual_journey\themes\hugo-eureka\resources\_gen\images\images\
-# it is a png formate.
+# it is a png format.
 # the image file at the homepage is stored in .\themes\hugo-eureka\assets\images. 
 # Then I need to change the previous one at D:\A_Blog\psalm119\config\_default\ params.yaml.
 # change footer at D:\A_Blog\psalm119\themes\hugo-eureka\layouts\partials\footer.html
@@ -70,7 +71,7 @@ blogdown::find_hugo()
 
 #----
 # To upload a batch of files
-# in the 'Terminal' tab next to the 'Console' tabe
+# in the 'Terminal' tab next to the 'Console' tab
 # copy and paste: git config --global user.name "lennyleadership"
 # copy and paste: git config --global user.email "lenny.leadership@hotmail.com"
 # copy and paste: git add -A
@@ -78,8 +79,8 @@ blogdown::find_hugo()
 
 
 # Procedure to build a new blog ----
-# Create a new repository.
-# Create a new project on the local computer.
+# Create a new repository at Github.
+# Create a new project on the local computer through version control.
 # execute blogdown::new_site(theme = "###/theme name") in console
 # commit to github if error messages are shown when building site.
 # setup at netlify.[refer to https://www.youtube.com/watch?v=9Jqvaoeh1W4&t=1081s]
